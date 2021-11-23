34825f26ef6205de1307a345b26b97288f428119
Martin Bernardi (Windows) <martin@mbernardi.com.ar>
2021-11-23 12:03:29 -0800

    Create and track multiple prizes

--- Assets/HuntGameControllerScript.cs
+++ Assets/HuntGameControllerScript.cs
@@ -6,38 +6,69 @@ using UnityEngine.XR.WSA.Input;
 public class HuntGameControllerScript : MonoBehaviour
 {
     public GameObject clue;
-    public GameObject prize;
-    private PrizeScript prizeScript;
+    public GameObject prizePrefab;
     public Color farClueColor = new Color(1f, 1f, 1f, 0.4f); // Transparent White
     public Color closeClueColor = new Color(1f, 0f, 0f, 0.4f); // Transparent Red
     public float maxClueDist = 6; // 6 meters, the distance where the clue will be of color farClueColor
 
+    private List<GameObject> prizes;
+    private GameObject prize = null;
+    private PrizeScript prizeScript;
+
     void Start()
     {
+        prizes = new List<GameObject>();
+        prizes.Add((GameObject)Instantiate(prizePrefab, new Vector3(1, 0, 2), Quaternion.identity));
+        prizes.Add((GameObject)Instantiate(prizePrefab, new Vector3(1, 0, 0), Quaternion.identity));
+        prizes.Add((GameObject)Instantiate(prizePrefab, new Vector3(1, 0, -2), Quaternion.identity));
+       
         clue.SetActive(true);
-        prize.SetActive(true);
 
         InteractionManager.InteractionSourceUpdatedLegacy += HandUpdated;
         clue.transform.position = new Vector3(0, 0, 0);
+    }
+
+    /// <summary>
+    /// Start tracking a random prize
+    /// </summary>
+    private void SelectPrize()
+    {
+        int i = Random.Range(0, prizes.Count);
+        prize = prizes[i];
         prizeScript = prize.GetComponent<PrizeScript>();
-        //prizeScript.ActAsFound();
     }
 
     private void HandUpdated(UnityEngine.XR.WSA.Input.InteractionSourceState state)
     {
-        Vector3 pos;
-        if (state.sourcePose.TryGetPosition(out pos))
+
+        if (prize != null)
         {
-            clue.transform.position = pos;
-            clue.transform.LookAt(prize.transform.position);
-
-            float dist = Vector3.Distance(pos, prize.transform.position);
-            clue.GetComponent<Renderer>().material.color = Color.Lerp(closeClueColor, farClueColor, dist / maxClueDist);
-
-            if (dist < 0.3)
+            Vector3 pos;
+            if (state.sourcePose.TryGetPosition(out pos))
             {
-                prizeScript.ActAsFound();
+                clue.transform.position = pos;
+                
+                float dist = Vector3.Distance(pos, prize.transform.position);
+                clue.GetComponent<Renderer>().material.color = Color.Lerp(closeClueColor, farClueColor, dist / maxClueDist);
+
+                if (dist < 0.3)
+                {
+                    prizeScript.ActAsFound();
+                }
             }
+        }
+    }
+
+    void Update()
+    {
+        if (prize == null)
+        {
+            SelectPrize();
+            //prizeScript.ActAsFound();
+        }
+        else
+        {
+            clue.transform.LookAt(prize.transform.position);
         }
     }
 }
